c0398ada55d70b478a8e0d8904e2514b72dde971
ycalderono <yeissoncalderonortiz@gmail.com>
2024-06-28 22:08:34 -0500

    Agregando el redireccionamiento de promociones

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
                                 <Link href="/ajustes" size="lg" className="text-white">Ajustes</Link>
                             </NavbarMenuItem>
                             <NavbarMenuItem>
-                                <Link href="/promociones" size="lg" style={{ color: '#fe41f0' }}>Promociones</Link>
+                                <Link href="/landing" size="lg" style={{ color: '#fe41f0' }}>Promociones</Link>
                             </NavbarMenuItem>
                             <NavbarMenuItem>
                                 <Link href="/ayuda" size="lg" className="text-white">Ayuda</Link>
